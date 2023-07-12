ef49963aefa91b6175d8024acf8e2ded1cb7d7f6
raju-2003 <99482541+raju-2003@users.noreply.github.com>
2023-07-12 15:20:10 +0530

    Add files via upload

---
+++ Day 5/Homeimage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Homeimage.css';
+import Contents from './Contents';
+import { useState , useEffect } from 'react';
+
+const Home = () => {
+  const [index , setIndex] = useState(0);
+  const images = [
+    "best-health-insurance-plans.jpg",
+    "health-insurance-0.jpeg",
+    "insurance.jpg",
+    "health-insurance-image.jpg",
+  ]
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((index) => index + 1);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="split-page">
+      <div className="image-section">
+        <img src={images[index % images.length]} alt="health-insurance" width={600} height={400}/>
+      </div>
+      <Contents />
+    </div>
+  );
+};
+
+export default Home;
